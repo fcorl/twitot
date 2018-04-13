56c2c7939be0556b01b1152b2b8fb19135fdc3ea
fcorl <freddiecorless7@gmail.com>
2018-04-13 12:23:58 +0100

    Add files via upload

---
+++ twit/examples/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key:         'TQQEGRGlSZa27RDL8A9KHV1Dw',
+  consumer_secret:      'K2fmdyeunEcNpOyJN3EhlPY5bd6z0eX0LW5aR1ivsaTeoHK10S',
+  access_token:         '862317140258553858-THmLUvOBM34LDkqvZQO9xeplHyplQrV',
+  access_token_secret:  'ARUG3uyWGEzIlBx4QM7sVdgEogwKos5lOEHFzGbb116cY',
+ }
