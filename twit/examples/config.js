6501959cafac341fc591fea093b990ee8ff873ce
fcorl <freddiecorless7@gmail.com>
2018-04-25 14:37:29 +0100

    update Oauth keys for new app in config file

--- twit/examples/config.js
+++ twit/examples/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         'TQQEGRGlSZa27RDL8A9KHV1Dw',
-  consumer_secret:      'K2fmdyeunEcNpOyJN3EhlPY5bd6z0eX0LW5aR1ivsaTeoHK10S',
-  access_token:         '862317140258553858-THmLUvOBM34LDkqvZQO9xeplHyplQrV',
-  access_token_secret:  'ARUG3uyWGEzIlBx4QM7sVdgEogwKos5lOEHFzGbb116cY',
+  consumer_key:         'JV964gkzVUpe0CvJOwzWbMZSo',
+  consumer_secret:      'oH0id6999m0rrGhHQxEFiThUTq6aQIqFMam96kgRsZOpbUBnZf',
+  access_token:         '862317140258553858-clks8VEz3pB4dRIdYyduYqWW689IvxN',
+  access_token_secret:  'frBpX90mSB4sbBWMPGnndU1XQH9OziUJ0SMroMJRUw5tn',
  }
